04ef181ba867cf6a75adf822e85b6327cfde9167
Steve Jonk <stevejonk1993@gmail.com>
2023-07-25 17:06:21 +0200

    style: change colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,9 +32,9 @@ module.exports = {
         sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.teal,
-        //@ts-ignore
-        gray: colors.neutral, // TODO: Remove ts-ignore after tw types gets updated to v3
+        primary: colors.blue,
+        secondary: colors.red,
+        gray: colors.neutral,
       },
       typography: (theme) => ({
         DEFAULT: {
