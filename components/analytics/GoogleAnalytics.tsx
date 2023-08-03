ed4cd2951a5bb8a8ee39aac12e641ca84a7354d1
Steve Jonk <stevejonk1993@gmail.com>
2023-08-03 12:15:08 +0200

    feat: add google analytics

--- components/analytics/GoogleAnalytics.tsx
+++ components/analytics/GoogleAnalytics.tsx
@@ -6,18 +6,15 @@ const GAScript = () => {
   return (
     <>
       <Script
-        strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=${siteMetadata.analytics.googleAnalyticsId}`}
       />
-
-      <Script strategy="lazyOnload" id="ga-script">
+      <Script id="google-analytics">
         {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${siteMetadata.analytics.googleAnalyticsId}', {
-              page_path: window.location.pathname,
-            });
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${siteMetadata.analytics.googleAnalyticsId}');
         `}
       </Script>
     </>
