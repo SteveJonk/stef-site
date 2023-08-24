907378597f10ff3da905f6e33d564b30bb2486e2
Steve Jonk <stevejonk1993@gmail.com>
2023-08-24 11:04:36 +0200

    fix: scroll to on chrome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps, router }: AppProps) {
       {isDevelopment && isSocket && <ClientReload />}
       <Analytics />
       <LayoutWrapper>
-        <AnimatePresence mode="wait" initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
+        <AnimatePresence mode="wait" initial={false} onExitComplete={() => window.scroll(0, 0)}>
           <Component {...pageProps} key={router.asPath} />
         </AnimatePresence>
       </LayoutWrapper>
